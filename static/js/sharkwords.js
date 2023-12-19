6dcf45bd0ed58dea262d0cb2ed87a12cd85989bc
Nadia Hina <nadianadia062@gmail.com>
2023-12-19 13:04:57 -0800

    added files

---
+++ static/js/sharkwords.js
@@ -0,0 +1,87 @@
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+
+const WORDS = [
+  'strawberry',
+  'orange',
+  'apple',
+  'banana',
+  'pineapple',
+  'kiwi',
+  'peach',
+  'pecan',
+  'eggplant',
+  'durian',
+  'peanut',
+  'chocolate',
+];
+
+const numWrong = 0;
+
+// Loop over the letters in `word` and create divs.
+// The divs should be appended to the section with id="word-container".
+//
+// Use the following template string to create each div:
+// `<div class="letter-box ${letter}"></div>`
+//
+const createDivsForChars = (word) => {
+  // loop over each letter of our word
+  // for each letter, create a dive
+  //append div to word-container using insertAdjacentHTML
+  const wordContainer = document.querySelector("#word-container");
+  for(const letter of word){
+    wordContainer.insertAdjacentHTML(
+      'beforeend', 
+      `<div class="letter-box ${letter}"></div>`);
+  }
+}
+
+// Loop over each letter in the alphabet and generate a button for each letter
+// The buttons should be appended to the section with id="letter-buttons".
+const generateLetterButtons = () => {
+  // access letter-buttons section, set to a variable
+  //loop over alphabet 
+  //for each letter
+    //add a button
+    const wordContainer = document.querySelector("#letter-buttons");
+    for (letter of ALPHABET){
+      wordContainer.insertAdjacentHTML('beforeend', `<button>${letter}</button>`)
+    }
+};
+
+// Set the `disabled` property of `buttonEl` to true.
+//
+// `buttonEl` is an `HTMLElement` object.
+//
+const disableLetterButton = (buttonEl) => {
+  // Replace this with your code
+  buttonEl.setAttribute('disabled', 'true');
+};
+
+// This is a helper function we will use in the future
+// It should return `true` if `letter` is in the word
+// For now, you should test it out to make sure it works
+
+const isLetterInWord = (letter, word) => {
+  // Replace this with your code
+};
+
+// This is like if __name__ == '__main__' in Python
+// It will be called when the file is run (because
+// we call the function on line 66)
+(function startGame() {
+  // For now, we'll hardcode the word that the user has to guess
+  // You can change this to choose a random word from WORDS once you
+  // finish this lab but we hard code it so we know what the word is
+  // and can tell if things look correct for this word
+  const word = 'hello';
+
+  createDivsForChars(word);
+
+  generateLetterButtons();
+
+  const buttonA = document.querySelector('button');
+  console.log(buttonA)
+  disableLetterButton(buttonA)
+  // in the next lab, you will be adding functionality to handle when
+  // someone clicks on a letter
+})();
